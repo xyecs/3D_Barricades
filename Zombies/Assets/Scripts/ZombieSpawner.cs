1cbd30837255fc8bd2949b6b1e82a409ee302807
xyecs <runedesired2@gmail.com>
2017-06-08 17:20:03 -0400

    setup scripts for zombies

--- Zombies/Assets/Scripts/ZombieSpawner.cs
+++ Zombies/Assets/Scripts/ZombieSpawner.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class ZombieSpawner : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+	public GameObject ZombieToSpawn;
+	public int ZombieNumbers; // tracks total amount spawned	
 		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		
+		Spawn();
+	}
+	
+	IEnumerator Spawn() {
+		// test, will spawn 1 every 5sec
+		Debug.Log("Spawned");
+		ZombieNumbers += 1;
+		yield return new WaitForSeconds(5);
 	}
 }
