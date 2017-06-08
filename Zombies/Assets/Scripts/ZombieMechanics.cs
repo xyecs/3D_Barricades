eaf52df57c7bd94889866e78642ec632488d1c95
xyecs <runedesired2@gmail.com>
2017-06-08 16:41:10 -0400

    zombie follows player, needs animation, adding spawners

--- Zombies/Assets/Scripts/ZombieMechanics.cs
+++ Zombies/Assets/Scripts/ZombieMechanics.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class ZombieMechanics : MonoBehaviour {
 
 	public GameObject ThePlayer;
-	public GameObject TheEnemy;
+	public GameObject TheZombie;
 	public float TargetDistance;
-	public float AllowedRange = 10;
-	public RaycastHit hit;
+	public RaycastHit Hit;
+	public int Status; // 0 = not in range, move to player, 1 = in-range of player can attack
+	public float ZombieSpeed = 0.02f;	
 	
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(ThePlayer.transform);
+		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)){
+			transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, ZombieSpeed);
+		}
 	}
 }
