35f97bfdb2cdc8402d80910849f35f2708656550
xyecs <runedesired2@gmail.com>
2017-06-08 16:17:25 -0400

    import zombie + script setup

---
+++ Zombies/Assets/Scripts/ZombieMechanics.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieMechanics : MonoBehaviour {
+
+	public GameObject ThePlayer;
+	public GameObject TheEnemy;
+	public float TargetDistance;
+	public float AllowedRange = 10;
+	public RaycastHit hit;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(ThePlayer.transform);
+	}
+}
