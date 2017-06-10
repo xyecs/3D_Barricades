7f40c410e85ca1c25c8c2cb11ea323b2ed475801
xyecs <runedesired2@gmail.com>
2017-06-10 16:33:09 -0400

    attack trigger for zombies added

--- Zombies/Assets/Scripts/ZombieMechanics.cs
+++ Zombies/Assets/Scripts/ZombieMechanics.cs
@@ -8,14 +8,26 @@ public class ZombieMechanics : MonoBehaviour {
 	public GameObject TheZombie;
 	public float TargetDistance;
 	public RaycastHit Hit;
-	public int Status; // 0 = not in range, move to player, 1 = in-range of player can attack
+	public int ZombieStatus; // 0 = not in range, move to player, 1 = in-range of player can attack
 	public float ZombieSpeed = 0.02f;	
 	
 	// Update is called once per frame
 	void Update () {
 		transform.LookAt(ThePlayer.transform);
-		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)){
-			transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, ZombieSpeed);
-		}
+		if (ZombieStatus == 0) {
+			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)){
+				transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, ZombieSpeed);
+			}
+		} 
+	}
+	
+	void OnTriggerEnter () {
+		ZombieStatus = 1;
+		ZombieSpeed = 0;	
+	}
+	
+	void OnTriggerExit () {
+		ZombieStatus = 0;
+		ZombieSpeed = 0.1f;	
 	}
 }
