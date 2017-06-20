67aa3a071fea0803d346900895b527eec3c01523
xyecs <runedesired2@gmail.com>
2017-06-19 22:48:50 -0400

    animated zombie also changed its assets

--- Zombies/Assets/Scripts/ZombieMechanics.cs
+++ Zombies/Assets/Scripts/ZombieMechanics.cs
@@ -9,25 +9,34 @@ public class ZombieMechanics : MonoBehaviour {
 	public float TargetDistance;
 	public RaycastHit Hit;
 	public int ZombieStatus; // 0 = not in range, move to player, 1 = in-range of player can attack
-	public float ZombieSpeed = 0.02f;	
+	public float ZombieSpeed = 0.1f;	
+	Animator ToonZombieAnimator;
 	
-	// Update is called once per frame
+	void Start () {
+		ToonZombieAnimator = GetComponent<Animator>();
+	}
+	
 	void Update () {
 		transform.LookAt(ThePlayer.transform);
 		if (ZombieStatus == 0) {
+			ZombieSpeed = 0.1f;	
+			ToonZombieAnimator.SetBool("IsAttacking", false);
 			if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out Hit)){
 				transform.position = Vector3.MoveTowards(transform.position, ThePlayer.transform.position, ZombieSpeed);
 			}
-		} 
+		} else {
+			ToonZombieAnimator.SetBool("IsAttacking", true);
+			ZombieSpeed = 0;	
+		}
 	}
 	
 	void OnTriggerEnter () {
+		Debug.Log("Zombie Entered");
 		ZombieStatus = 1;
-		ZombieSpeed = 0;	
 	}
 	
 	void OnTriggerExit () {
+		Debug.Log("Zombie Exited");
 		ZombieStatus = 0;
-		ZombieSpeed = 0.1f;	
 	}
 }
